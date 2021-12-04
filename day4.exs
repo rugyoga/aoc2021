8f9c32a349edd411ef57fafe85010fa789afbbe5
Guy Argo <guy@Guys-M1-Air.local>
2021-12-03 21:56:16 -0800

    day4

---
+++ day4.exs
@@ -0,0 +1,28 @@
+defmodule Day4 do
+  def play([number | numbers], boards) do
+    boards = boards |> Enum.map(&mark_board(number, &1))
+    done = Enum.filter(boards, &complete?/1)
+    if Enum.empty?(done) do
+      play(numbers, boards)
+    else
+      done |> List.first |> List.flatten |> Enum.filter(&(&1 != "M")) |> Enum.sum |> then(&(&1 * number))
+    end
+  end
+
+  def mark_board(number, board), do: Enum.map(board, &mark_row(number, &1))
+
+  def mark_row(_, []), do: []
+  def mark_row(number, [n | row]) when n == number, do: ["M" | row]
+  def mark_row(number, [n | row]), do: [n | mark_row(number, row)]
+
+  def complete?(board), do: Enum.any?(board, fn row -> Enum.all?(row, &(&1 == "M")) end) ||
+                            Enum.any?(Enum.zip_with(board, &(&1)), fn col -> Enum.all?(col, &(&1 == "M")) end)
+end
+
+[numbers | boards] = "day4.txt" |> File.read! |> String.split("\n\n", trim: true)
+numbers = numbers |> String.split(",", trim: true) |> Enum.map(&String.to_integer/1)
+boards = 
+  boards
+  |> Enum.map(fn b -> b |> String.split("\n", trim: true) |> Enum.map(fn r -> r |> String.split(" ", trim: true) |> Enum.map(&String.to_integer/1) end) end)
+
+Day4.play(numbers, boards) |> IO.inspect
