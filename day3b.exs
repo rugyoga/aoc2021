44bedee8db75b90f61a2fb9deb9d50b247bea828
Guy Argo <guy.argo@gmail.com>
2021-12-03 16:56:29 -0800

    Update day3b.exs
    DRY by rolling up carbon and oxygen

--- day3b.exs
+++ day3b.exs
@@ -6,8 +6,7 @@ defmodule Day3 do
     bits |> Enum.filter(&(get.(&1) == target)) |> finder(f, i+1)
   end
 
-  def oxygen(freqs), do: if map_size(freqs) == 1, do: Map.keys(freqs) |> List.first, else: if freqs[1] >= freqs[0], do: 1, else: 0
-  def carbon(freqs), do: if map_size(freqs) == 1, do: Map.keys(freqs) |> List.first, else: if freqs[0] <= freqs[1], do: 0, else: 1
+  def p(freqs, a, b), do: if map_size(freqs) == 1, do: Map.keys(freqs) |> List.first, else: if freqs[0] <= freqs[1], do: a, else: b
 end
 
 bits = "day3.txt"
@@ -15,4 +14,4 @@ bits = "day3.txt"
 |> String.split("\n", trim: true)
 |> Enum.map(fn s -> s |> String.split("", trim: true) |> Enum.map(&String.to_integer/1) end)
 
-IO.inspect(Day3.finder(bits, &Day3.oxygen/1, 0) * Day3.finder(bits, &Day3.carbon/1, 0))
+IO.inspect(Day3.finder(bits, &Day3.p(&1, 1, 0), 0) * Day3.finder(bits, &&Day3.p(&1, 0, 1), 0))
