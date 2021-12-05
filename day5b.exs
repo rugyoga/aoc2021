072bce9152ae9b7ff2194c0204660ed2eb0e7cea
Guy Argo <guy.argo@gmail.com>
2021-12-04 21:47:41 -0800

    Day 5 part 2

---
+++ day5b.exs
@@ -0,0 +1,38 @@
+defmodule Day5 do
+  def update(position, counts), do: Map.update(counts, position, 1, &(&1+1))
+end
+
+"day5.txt"
+ |> File.read!
+ |> String.split("\n", trim: true)
+ |> Enum.map(fn x -> 
+                x 
+                |> String.split(" -> ", trim: true)
+                |> Enum.map(fn pair -> pair |> String.split(",", trim: true) |> Enum.map(&String.to_integer/1) end)
+                end)
+ |> Enum.reduce(
+   %{},
+   fn [[x1, y1], [x2, y2]], counts ->
+     if x1 == x2 do
+        step = if y1 < y2, do: 1, else: -1
+        for y <- y1..y2//step, do: [x1, y]
+     else
+      if y1 == y2 do
+          step = if x1 < x2, do: 1, else: -1
+          for x <- x1..x2//step, do: [x, y1]
+      else
+        step_x = if x1 < x2, do: 1, else: -1
+        step_y = if y1 < y2, do: 1, else: -1
+        Enum.zip_with(
+          (for x <- x1..x2//step_x, do: x),
+          (for y <- y1..y2//step_y, do: y),
+          fn x, y -> [x, y] end
+        )
+      end
+     end
+     |> Enum.reduce(counts, &Day5.update/2)
+   end
+ )
+ |> Enum.filter(fn {_, count} -> count >= 2 end)
+ |> length
+ |> IO.inspect
