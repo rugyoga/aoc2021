c497c8d4f4cae1e5f008d951a8f26012039fb11f
Guy Argo <guy.argo@lob.com>
2021-12-06 21:39:08 -0800

    one-liners

--- day7.exs
+++ day7.exs
@@ -3,9 +3,6 @@
  |> String.trim
  |> String.split(",")
  |> Enum.map(&String.to_integer/1)
- |> then(fn l ->
-  {min, max} = Enum.min_max(l)
-  Enum.map(min..max, fn col -> l |> Enum.map(&(abs(col - &1))) |> Enum.sum end)
- end)
+ |> then(fn l -> {min, max} = Enum.min_max(l); Enum.map(min..max, fn col -> l |> Enum.map(&(abs(col - &1))) |> Enum.sum end) end)
  |> Enum.min
  |> IO.inspect
--- day7b.exs
+++ day7b.exs
@@ -5,9 +5,6 @@ f = fn n -> div(n * (n+1), 2) end
  |> String.trim
  |> String.split(",")
  |> Enum.map(&String.to_integer/1)
- |> then(fn l ->
-  {min, max} = Enum.min_max(l)
-  Enum.map(min..max, fn col -> l |> Enum.map(&(f.(abs(col - &1)))) |> Enum.sum end)
- end)
+ |> then(fn l -> {min, max} = Enum.min_max(l); Enum.map(min..max, fn col -> l |> Enum.map(&(f.(abs(col - &1)))) |> Enum.sum end) end)
  |> Enum.min
  |> IO.inspect
