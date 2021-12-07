10e8d73196563e366581778350fbee89cebc7725
Guy Argo <guy.argo@lob.com>
2021-12-06 21:10:53 -0800

    day 7 part 1

---
+++ day7.exs
@@ -0,0 +1,12 @@
+"day7.txt"
+ |> File.read!
+ |> String.trim
+ |> String.split(",")
+ |> Enum.map(&String.to_integer/1)
+ |> then(fn l ->
+  min = Enum.min(l)
+  max = Enum.max(l)
+  Enum.map(min..max, fn col -> l |> Enum.map(&(abs(col - &1))) |> Enum.sum end)
+ end)
+ |> Enum.min
+ |> IO.inspect
