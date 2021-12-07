a1081041055163aa7ad611f904dfd0982f071595
Guy Argo <guy.argo@gmail.com>
2021-12-06 21:29:18 -0800

    Update day7b.exs
    better name

--- day7b.exs
+++ day7b.exs
@@ -1,4 +1,4 @@
-x = fn n -> n * (n+1)/2 end
+f = fn n -> n * (n+1)/2 end
 
 "day7.txt"
  |> File.read!
@@ -8,7 +8,7 @@ x = fn n -> n * (n+1)/2 end
  |> then(fn l ->
   min = Enum.min(l)
   max = Enum.max(l)
-  Enum.map(min..max, fn col -> l |> Enum.map(&(x.(abs(col - &1)))) |> Enum.sum end)
+  Enum.map(min..max, fn col -> l |> Enum.map(&(f.(abs(col - &1)))) |> Enum.sum end)
  end)
  |> Enum.min
  |> IO.inspect
