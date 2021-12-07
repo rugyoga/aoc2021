a168aeb798140a88385acadedba9fc1de2601b5b
Guy Argo <guy.argo@gmail.com>
2021-12-06 21:32:10 -0800

    Update day7.exs
    Use min_max

--- day7.exs
+++ day7.exs
@@ -4,8 +4,7 @@
  |> String.split(",")
  |> Enum.map(&String.to_integer/1)
  |> then(fn l ->
-  min = Enum.min(l)
-  max = Enum.max(l)
+  {min, max} = Enum.min_max(l)
   Enum.map(min..max, fn col -> l |> Enum.map(&(abs(col - &1))) |> Enum.sum end)
  end)
  |> Enum.min
