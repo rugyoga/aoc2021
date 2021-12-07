1669eabb5f05c94da0954101dccd0db94346861a
Guy Argo <guy.argo@gmail.com>
2021-12-06 21:31:12 -0800

    Update day7b.exs
    Use min_max

--- day7b.exs
+++ day7b.exs
@@ -6,8 +6,7 @@ f = fn n -> n * (n+1)/2 end
  |> String.split(",")
  |> Enum.map(&String.to_integer/1)
  |> then(fn l ->
-  min = Enum.min(l)
-  max = Enum.max(l)
+  {min, max} = Enum.min_max(l)
   Enum.map(min..max, fn col -> l |> Enum.map(&(f.(abs(col - &1)))) |> Enum.sum end)
  end)
  |> Enum.min
