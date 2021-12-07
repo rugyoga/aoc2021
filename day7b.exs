25e35a4a1e66b94c020298eea62cd1a339a22db9
Guy Argo <guy.argo@lob.com>
2021-12-06 21:14:56 -0800

    Day 7 part 2

---
+++ day7b.exs
@@ -0,0 +1,14 @@
+x = fn n -> n * (n+1)/2 end
+
+"day7.txt"
+ |> File.read!
+ |> String.trim
+ |> String.split(",")
+ |> Enum.map(&String.to_integer/1)
+ |> then(fn l ->
+  min = Enum.min(l)
+  max = Enum.max(l)
+  Enum.map(min..max, fn col -> l |> Enum.map(&(x.(abs(col - &1)))) |> Enum.sum end)
+ end)
+ |> Enum.min
+ |> IO.inspect
