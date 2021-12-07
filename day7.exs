fa7ad62cf985dd33d6352f3da454ed9f546a310b
Guy Argo <guy.argo@lob.com>
2021-12-06 21:42:40 -0800

    remove unnecessary trim

--- day7.exs
+++ day7.exs
@@ -1,6 +1,5 @@
 "day7.txt"
  |> File.read!
- |> String.trim
  |> String.split(",")
  |> Enum.map(&String.to_integer/1)
  |> then(fn l -> {min, max} = Enum.min_max(l); Enum.map(min..max, fn col -> l |> Enum.map(&(abs(col - &1))) |> Enum.sum end) end)
--- day7b.exs
+++ day7b.exs
@@ -2,7 +2,6 @@ f = fn n -> div(n * (n+1), 2) end
 
 "day7.txt"
  |> File.read!
- |> String.trim
  |> String.split(",")
  |> Enum.map(&String.to_integer/1)
  |> then(fn l -> {min, max} = Enum.min_max(l); Enum.map(min..max, fn col -> l |> Enum.map(&(f.(abs(col - &1)))) |> Enum.sum end) end)
