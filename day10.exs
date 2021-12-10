32f58cf8210164d58f0337239f22bf375de2bb8e
Guy Argo <guy.argo@lob.com>
2021-12-10 10:22:27 -0800

    tidy up day 10

--- day10.exs
+++ day10.exs
@@ -1,30 +1,17 @@
 defmodule Day10 do
-    @match %{"{" => "}",
-             "[" => "]",
-             "(" => ")",
-             "<" => ">"}
-    @value %{ ")" => 3,
-              "]" => 57,
-             "}" => 1197,
-             ">" => 25137}
+    @match %{"{" => "}", "[" => "]", "(" => ")", "<" => ">"}
+    @value %{ ")" => 3, "]" => 57, "}" => 1197, ">" => 25137}
     def find_error([], _), do: nil
     def find_error([x | xs], [y | ys]) when x == y, do: find_error(xs, ys)
-    def find_error([x | xs], ys) do
-        if Map.has_key?(@match, x) do
-          find_error(xs, [@match[x] | ys])
-        else
-          x
-        end
-    end
-    
+    def find_error([x | xs], ys), do: if Map.has_key?(@match, x), do: find_error(xs, [@match[x] | ys]), else: x
+
     def score(x), do: @value[x]
 end
 
 "day10.txt"
 |> File.read!
 |> String.split("\n", trim: true)
-|> Enum.map(&String.codepoints/1)
-|> Enum.map(&Day10.find_error(&1, []))
+|> Enum.map(&(&1 |> String.codepoints |> Day10.find_error([])))
 |> Enum.filter(&(&1 != nil))
 |> Enum.map(&Day10.score/1)
 |> Enum.sum
--- day10b.exs
+++ day10b.exs
@@ -1,14 +1,8 @@
-defmodule Day10 do
+defmodule Day10b do
     @match %{"{" => "}", "[" => "]", "(" => ")", "<" => ">"}
     def find_error([], ys), do: ys
     def find_error([x | xs], [y | ys]) when x == y, do: find_error(xs, ys)
-    def find_error([x | xs], ys) do
-        if Map.has_key?(@match, x) do
-          find_error(xs, [@match[x] | ys])
-        else
-          nil
-        end
-    end
+    def find_error([x | xs], ys), do: if Map.has_key?(@match, x), do: find_error(xs, [@match[x] | ys]), else: nil
 end
 
 value = %{ ")" => 1, "]" => 2, "}" => 3, ">" => 4}
@@ -16,8 +10,7 @@ value = %{ ")" => 1, "]" => 2, "}" => 3, ">" => 4}
 "day10.txt"
 |> File.read!
 |> String.split("\n", trim: true)
-|> Enum.map(&String.codepoints/1)
-|> Enum.map(&Day10.find_error(&1, []))
+|> Enum.map(&(&1 |> String.codepoints |> Day10b.find_error([])))
 |> Enum.filter(&(&1 != nil))
 |> Enum.map(&Enum.reduce(&1, 0, fn x, v -> v*5 + value[x] end))
 |> Enum.sort
