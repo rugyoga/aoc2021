62de537c5be197a60add90f59002abc46e0b6c61
Guy Argo <guy.argo@lob.com>
2021-12-11 15:12:05 -0800

    day 9 snapshot

--- day9b.exs
+++ day9b.exs
@@ -1,31 +1,23 @@
-defmodule Day10 do
-    @match %{"{" => "}",
-             "[" => "]",
-             "(" => ")",
-             "<" => ">"}
-    @value %{ ")" => 3,
-              "]" => 57,
-             "}" => 1197,
-             ">" => 25137}
-    def find_error([], _), do: nil
-    def find_error([x | xs], [y | ys]) when x == y, do: find_error(xs, ys)
-    def find_error([x | xs], ys) do
-        if Map.has_key?(@match, x) do
-          find_error(xs, [@match[x] | ys])
-        else
-          x
-        end
-    end
-    
-    def score(x), do: @value[x]
+defmodule Day9b do
+  def low_point?({{x, y}, value}, map) do
+    [{x+1, y}, {x-1, y}, {x, y+1}, {x, y-1}]
+    |> Enum.filter(&Map.has_key?(map, &1))
+    |> Enum.all?(&(Map.get(map, &1) > value))
+  end
+
+  def basin(low_point, map) do
+    {basin, map}
+  end
 end
 
-"day10.txt"
-|> File.read!
-|> String.split("\n", trim: true)
-|> Enum.map(&String.codepoints/1)
-|> Enum.map(&Day10.find_error(&1, []))
-|> Enum.filter(&(&1 != nil))
-|> Enum.map(&Day10.score/1)
-|> Enum.sum
-|> IO.inspect
+by_position =
+  "day9.txt"
+  |> File.read!
+  |> String.split("\n", trim: true)
+  |> Enum.map(fn line -> line |> String.split("", trim: true) |> Enum.map(&String.to_integer/1) |> Enum.with_index end)
+  |> Enum.with_index
+  |> Enum.map(fn {list, row} -> list |> Enum.map(fn {num, col} -> {{row, col}, num} end) end)
+  |> List.flatten
+  |> Map.new
+
+low_points = by_position |> Enum.filter(&low_point?.(&1, by_position))
