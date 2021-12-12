7f924cd73abf2077bbd8c5c14ba0ab50c6ec5d5c
Guy Argo <guy.argo@lob.com>
2021-12-11 18:32:55 -0800

    Day 9 part 2 snapshot

--- day9b.exs
+++ day9b.exs
@@ -1,12 +1,16 @@
 defmodule Day9b do
-  def low_point?({{x, y}, value}, map) do
+  def neighbours({x, y}, map) do
     [{x+1, y}, {x-1, y}, {x, y+1}, {x, y-1}]
-    |> Enum.filter(&Map.has_key?(map, &1))
-    |> Enum.all?(&(Map.get(map, &1) > value))
+    |> Enum.filter(&(Map.has_key?(map, &1))
+    |> Enum.map(&{&1, map[&1]})
   end
 
-  def basin(low_point, map) do
-    {basin, map}
+  def upward({point, value}, map) do
+    neighbours(point) |> Enum.filter(&(elem(&1, 1) > value))
+  end
+
+  def expand(current, basin, map) do
+    upward(current, map) |> Enum.reduce(&expand(&1, &2, map))
   end
 end
 
@@ -18,6 +22,15 @@ by_position =
   |> Enum.with_index
   |> Enum.map(fn {list, row} -> list |> Enum.map(fn {num, col} -> {{row, col}, num} end) end)
   |> List.flatten
+  |> Enum.reject(fn {_, v} -> v == 9 end)
   |> Map.new
 
+low_points =
+  |> Enum.reduce(%{}, fn item, path -> basin(item, path, data) |> elem(1) end)
+  |> Enum.map(&elem(&1, 1))
+  |> Enum.frequencies()
+  |> Map.values()
+  |> Enum.sort()
+  |> Enum.take(-3)
+  |> Enum.product()
 low_points = by_position |> Enum.filter(&low_point?.(&1, by_position))
