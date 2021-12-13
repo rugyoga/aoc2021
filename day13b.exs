e6f64bb7f816069ed9c17a31b910df3a72af1710
Guy Argo <guy.argo@lob.com>
2021-12-13 15:43:59 -0800

    Tidy up day 13 part 2

--- day13b.exs
+++ day13b.exs
@@ -1,5 +1,5 @@
-defmodule Day13 do
-  def folder(folds, points) do
+defmodule Day13b do
+  def folder(points, folds) do
     folds |> Enum.reduce(points, fn {dir, n}, points -> points |> Enum.map(&fold(dir, n, &1)) |> MapSet.new end)
   end
 
@@ -7,30 +7,21 @@ defmodule Day13 do
   def fold("y", n, {x, y} = p), do: if y > n, do: {x, n - (y-n)}, else: p
 end
 
-[coords, folds] =
-  "day13.txt"
-  |> File.read!
-  |> String.split("\n\n", trim: true)
-
- coords =
-  coords
-  |> String.split("\n", trim: true)
-  |> Enum.filter(&String.match?(&1, ~r/\d+/))
-  |> Enum.map(fn line -> line |> String.split(",", trim: true) |> Enum.map(&String.to_integer/1) |> then(fn [x, y] -> {x, y} end) end)
-  |> MapSet.new
+[points, folds] = "day13.txt" |> File.read! |> String.split("\n\n", trim: true)
 
 folds =
   folds
   |> String.split("\n", trim: true)
-  |> Enum.map(fn line ->
-                line
-                |> String.split("=")
-                |> then(fn [x, num] -> {x |> String.codepoints |> Enum.reverse |> List.first, String.to_integer(num)} end)
-  end)
+  |> Enum.map(&(&1 |> String.split("=") |> then(fn [x, num] -> {x |> String.codepoints |> List.last, String.to_integer(num)} end)))
 
-  points = Day13.folder(folds, coords)
-  {x, _} = points |> Enum.max_by(fn {x, _} -> x end)
-  {_, y} = points |> Enum.max_by(fn {_, y} -> y end)
+points =
+  points
+  |> String.split("\n", trim: true)
+  |> Enum.filter(&String.match?(&1, ~r/\d+/))
+  |> Enum.map(fn line -> line |> String.split(",", trim: true) |> Enum.map(&String.to_integer/1) |> List.to_tuple end)
+  |> Day13b.folder(folds)
 
-  Enum.map_join(0..y, "\n", fn y -> Enum.map_join(0..x, "", fn x -> (if MapSet.member?(points, {x, y}), do: "#", else: " ") end)  end)
-  |> IO.puts()
+{x, y} = points |> Enum.unzip |> then(fn {xs, ys} -> {Enum.max(xs), Enum.max(ys)} end)
+
+Enum.map_join(0..y, "\n", fn y -> Enum.map_join(0..x, "", fn x -> (if MapSet.member?(points, {x, y}), do: "#", else: " ") end)  end)
+|> IO.puts()
