11b5102c22af73b8ed02330b72e2ae902c0949fe
Guy Argo <guy.argo@lob.com>
2021-12-15 13:26:10 -0800

    Tidied up Day 12

--- day12.exs
+++ day12.exs
@@ -1,28 +1,25 @@
 defmodule Day12 do
-  def paths(x, path, graph, seen) do
-    graph[x]
-    |> Enum.reject(&MapSet.member?(seen, &1))
+  def paths(graph, lower, parent, path, seen) do
+    graph[parent]
+    |> Enum.reject(fn child -> MapSet.member?(seen, child) end)
     |> Enum.flat_map(
       fn "end" -> [["end" | path]]
-         x -> paths(x, [x| path], graph, (if lower?(x), do: MapSet.put(seen, x), else: seen))
+         child ->
+          seen = if MapSet.member?(lower, child), do: MapSet.put(seen, child), else: seen
+          paths(graph, lower, child, [child | path], seen)
       end)
   end
 
-  def lower?(s), do: s == String.downcase(s, :ascii)
+  def lower(graph), do: graph |> Map.keys |> Enum.filter(&(&1 == String.downcase(&1, :ascii))) |> MapSet.new
 
-  def add_node([a, b], map) do
-    map
-    |> Map.update(a, MapSet.new([b]), &MapSet.put(&1, b))
-    |> Map.update(b, MapSet.new([a]), &MapSet.put(&1, a))
-  end
+  def add_node([a, b], map), do: map |> Map.update(a, [b], &[b | &1]) |> Map.update(b, [a], &[a | &1])
 end
 
-paths =
-  "day12.txt"
-  |> File.read!
-  |> String.split("\n", trim: true)
-  |> Enum.map(&(String.split(&1, "-", trim: true)))
-
-graph = paths |> Enum.reduce(%{}, &Day12.add_node/2)
-
-Day12.paths("start", ["start"], graph, MapSet.new(["start"])) |> length |> IO.inspect
+"day12.txt"
+|> File.read!
+|> String.split("\n", trim: true)
+|> Enum.map(&String.split(&1, "-", trim: true))
+|> Enum.reduce(%{}, &Day12.add_node/2)
+|> then(&Day12.paths(&1, Day12.lower(&1), "start", ["start"], MapSet.new(["start"])))
+|> length
+|> IO.inspect
