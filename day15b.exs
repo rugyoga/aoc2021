ed5d71709699c911947b08652ecd5bad4467b6b0
Guy Argo <guy.argo@gmail.com>
2021-12-26 13:53:11 -0800

    Implementation of SkewHeap and solutions for Day 15 part 1 and 2

--- day15b.exs
+++ day15b.exs
@@ -1,4 +1,4 @@
-defmodule Day15 do
+defmodule Day15b do
   def neighbours({_, {x, y}}, visited, map) do
     [{x, y+1}, {x+1, y}, {x, y-1}, {x-1, y}]
     |> Enum.filter(&(Map.has_key?(map, &1) and !Map.has_key?(visited, &1)))
@@ -26,6 +26,14 @@ defmodule Day15 do
     end 
   end
 
+  def grids(row, col, num, n, k) do
+    for i <- 0..(k-1) do
+      for j <- 0..(k-1) do
+        {{i*n + row, j*n + col}, Integer.mod(num + i + j - 1, 9) + 1}
+      end
+    end
+  end
+
   def search(map, goal), do: search(SkewHeap.new() |> SkewHeap.push({0, {0,0}}), goal, %{}, map)
 end
 
@@ -34,8 +42,8 @@ end
   |> String.split("\n", trim: true)
   |> Enum.map(fn line -> line |> String.split("", trim: true) |> Enum.map(&String.to_integer/1) |> Enum.with_index end)
   |> Enum.with_index
-  |> Enum.map(fn {list, row} -> list |> Enum.map(fn {num, col} -> {{row, col}, num} end) end)
+  |> Enum.map(fn {list, row} -> list |> Enum.map(fn {num, col} -> Day15b.grids(row, col, num, 100, 5) end) end)
   |> List.flatten
   |> Map.new
-  |> Day15.search({99,99})
+  |> Day15b.search({499,499})
   |> IO.inspect
