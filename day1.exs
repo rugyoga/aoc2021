c0a0069416e788f5a726b5aa5596a9237c6a50a3
Guy Argo <guy.argo@lob.com>
2022-01-04 11:21:51 -0800

    testing soft links

--- day1.exs
+++ day1.exs
@@ -1,8 +1,1 @@
-"day1.txt"
-|> File.read!
-|> String.split("\n", trim: true)
-|> Enum.map(&String.to_integer/1)
-|> Enum.chunk_every(2, 1, :discard)
-|> Enum.filter(fn [a, b] -> a < b end)
-|> length
-|> IO.inspect
+day01.exs
