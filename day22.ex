b34641d281e726050fcb7862ae1d527bfebb1257
Guy Argo <guy.argo@lob.com>
2022-01-04 10:17:56 -0800

    fix syntax error

--- day22.ex
+++ day22.ex
@@ -3,11 +3,11 @@ defmodule Day22 do
         filename
         |> File.read!
         |> String.split("\n", trim: true)
-        |> Enum.map(fn line -> line 
+        |> Enum.map(fn line -> line
                                 |> String.split(" ", trim: true)
-                                |> then(fn [a, b] -> {a, b 
-                                                         |> String.split(",", trim: true) 
-                                                         |> Enum.map(fn line -> line 
+                                |> then(fn [a, b] -> {a, b
+                                                         |> String.split(",", trim: true)
+                                                         |> Enum.map(fn line -> line
                                                                                 |> String.split("=")
                                                                                 |> then(fn [var, range] -> {var, Code.eval_string(range) |> elem(0)} end) end)} end) end)
 
@@ -39,9 +39,6 @@ defmodule Day22 do
             end
         )
     end
-
-    def cube_intersection({x1, y1, z1})
-
 
     def process_cubes(lines) do
         map = %{ "on" => 1, "off" => 0}
