432581d44367254152aabca768fb374ce7f6f6ce
Guy Argo <guy.argo@lob.com>
2022-01-04 13:13:57 -0800

    Minor tweak to Day 22

--- day22.ex
+++ day22.ex
@@ -1,11 +1,12 @@
 defmodule Day22 do
     def data(filename \\ "day22.txt") do
+        verb = %{ "on" => 1, "off" => 0}
         filename
         |> File.read!
         |> String.split("\n", trim: true)
         |> Enum.map(fn line -> line
                                 |> String.split(" ", trim: true)
-                                |> then(fn [a, b] -> {a, b
+                                |> then(fn [a, b] -> {verb[a], b
                                                          |> String.split(",", trim: true)
                                                          |> Enum.map(fn line -> line
                                                                                 |> String.split("=")
@@ -18,18 +19,17 @@ defmodule Day22 do
     def ignore?(range), do: Range.disjoint?(@core_range, range)
 
     def process(lines) do
-        map = %{ "on" => 1, "off" => 0}
         lines
         |> Enum.reduce(
             %{},
-            fn {verb, [{"x", x_range}, {"y", y_range}, {"z", z_range}]}, core ->
+            fn {on, [{"x", x_range}, {"y", y_range}, {"z", z_range}]}, core ->
                 if ignore?(x_range) or ignore?(y_range) or ignore?(z_range) do
                     core
                 else
                     for x <- @core_range, Enum.member?(x_range, x) do
                         for y <- @core_range, Enum.member?(y_range, y) do
                             for z <- @core_range, Enum.member?(z_range, z) do
-                                {{x, y, z}, map[verb]}
+                                {{x, y, z}, on}
                             end
                         end
                     end
